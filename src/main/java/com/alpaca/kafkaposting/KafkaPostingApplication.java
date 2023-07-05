2e16d59303aeeadc407bc81ba804d07626939364
eunji.jang <jdmswl0525@gmail.com>
2023-07-05 17:28:48 +0900

    Initial Commit

---
+++ src/main/java/com/alpaca/kafkaposting/KafkaPostingApplication.java
@@ -0,0 +1,13 @@
+package com.alpaca.kafkaposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpaca/kafkaposting/KafkaPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpaca.kafkaposting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
